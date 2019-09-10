846bff974dea52e45208eb81508a27ceba77e8fd
shirleytwl <shirtanwl@gmail.com>
2019-09-10 10:46:23 +0800

    adjusted add category UI

--- src/client/components/packlist/users/list.jsx
+++ src/client/components/packlist/users/list.jsx
@@ -151,7 +151,6 @@ class List extends React.Component {
 					</CardColumns>
 					<div className="text-center">
                         <AddCategory tripId = {this.props.tripId} updateTripInfo={this.props.updateTripInfo}/>
-
 					</div>
 				</Col>
 			</Row>
